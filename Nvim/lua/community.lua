d3b1437788bdf41a886d0a73d765589c5326a031
kai <t@ttt.tl>
2024-10-17 14:38:19 +0800

    Nvim: update

--- Nvim/lua/community.lua
+++ Nvim/lua/community.lua
@@ -1,19 +1,23 @@
 ---@type LazySpec
 return {
-  "AstroNvim/astrocommunity",
-  -- theme
-  { import = "astrocommunity.colorscheme.everforest" },
+	"AstroNvim/astrocommunity",
+	-- theme
+	{ import = "astrocommunity.colorscheme.everforest" },
 
-  -- custom `everforest`
-  {
-    "everforest",
-    init = function()
-      vim.o.background = "light"
-      vim.g.everforest_background = "hard"
-    end,
-  },
+	-- custom `everforest`
+	{
+		"everforest",
+		init = function()
+			vim.o.background = "light"
+			vim.g.everforest_background = "hard"
+		end,
+	},
 
-  -- language
-  { import = "astrocommunity.pack.lua" },
-  { import = "astrocommunity.pack.json" },
+	-- language
+	{ import = "astrocommunity.pack.lua" },
+	{ import = "astrocommunity.pack.tailwindcss" },
+	{ import = "astrocommunity.pack.typescript" },
+	{ import = "astrocommunity.pack.yaml" },
+	{ import = "astrocommunity.pack.markdown" },
+	{ import = "astrocommunity.pack.json" },
 }
