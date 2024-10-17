0454ff66a4941de94fb8bb4f9bcb247c28ec0352
kai <t@ttt.tl>
2024-10-17 14:35:49 +0800

    Zsh: update

---
+++ Zsh/aliases.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# List
+alias l='ls -FG'
+alias la='ls -FGalh'
+alias ll='ls -FG1'
+
+# Proxy
+alias proxy='export https_proxy=http://127.0.0.1:7890 http_proxy=http://127.0.0.1:7890 all_proxy=socks5://127.0.0.1:789'
+alias unproxy='unset all_proxy http_proxy https_proxy'
+
+# Lock Screen
+alias lockscreen='pmset displaysleepnow'
+
+# Reload settings or hardware
+alias reload!="source $zshrc"
+alias reload="echo 'Use \"reload!\" instead'"
