8f8d1358aeb4040dfa9cff6d1009113e9ca9adf0
kai <t@ttt.tl>
2025-05-31 13:51:03 +0800

    update

--- Zsh/aliases.sh
+++ Zsh/aliases.sh
@@ -6,7 +6,7 @@ alias la='ls -FGalh'
 alias ll='ls -FG1'
 
 # Proxy
-alias proxy='export https_proxy=http://127.0.0.1:7890 http_proxy=http://127.0.0.1:7890 all_proxy=socks5://127.0.0.1:789'
+alias proxy='export https_proxy=http://127.0.0.1:6152;export http_proxy=http://127.0.0.1:6152;export all_proxy=socks5://127.0.0.1:6153'
 alias unproxy='unset all_proxy http_proxy https_proxy'
 
 # Lock Screen
